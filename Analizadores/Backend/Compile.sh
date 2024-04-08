c34cadfe1f6e05738d2c8d304183c92608eaac56
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2024-04-08 04:48:55 -0600

    Cup y Flex compilados sin problemas

--- Analizadores/Backend/Compile.sh
+++ Analizadores/Backend/Compile.sh
@@ -1,7 +1,19 @@
 echo "Compilacion de JFlex - Web Manager (Server)"
 java -jar /home/jbravo/Apps/jflex/jflex-1.9.1/lib/jflex-full-1.9.1.jar -d ../../WebManager_Server/src/main/java/com/jbrod/webmanager_server/analyzer/ WebManager.flex
 
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
 echo "Compilacion de cup"
-java -jar /home/jbravo/Apps/cup/cup-20160615/java-cup-11b.jar -parser SqlParser parser.cup
-mv SqlParser.java ../src/main/java/com/jbrod/ide_sql/app/analyzer/
-mv sym.java ../src/main/java/com/jbrod/ide_sql/app/analyzer/
+java -jar /home/jbravo/Apps/cup/cup-20160615/java-cup-11b.jar -parser WebParser WebManager.cup
+mv WebParser.java ../../WebManager_Server/src/main/java/com/jbrod/webmanager_server/analyzer/
+mv sym.java ../../WebManager_Server/src/main/java/com/jbrod/webmanager_server/analyzer/
