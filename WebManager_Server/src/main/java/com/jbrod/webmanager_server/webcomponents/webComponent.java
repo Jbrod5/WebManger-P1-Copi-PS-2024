dc98157e6aa3536e0611496847b4f0557f559582
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2024-04-11 04:32:32 -0600

    Componentes web

---
+++ WebManager_Server/src/main/java/com/jbrod/webmanager_server/webcomponents/webComponent.java
@@ -0,0 +1,16 @@
+
+package com.jbrod.webmanager_server.webcomponents;
+
+/**
+ * Clase que representa a un componente de una pagina web.
+ * Puede ser TITULO, PARRAFO, IMAGEN, VIDEO, MENU.
+ * @author jbravo
+ */
+public class webComponent {
+    private String htmlContent; 
+    
+    public String getHtml(){
+        return htmlContent;
+    }
+
+}
