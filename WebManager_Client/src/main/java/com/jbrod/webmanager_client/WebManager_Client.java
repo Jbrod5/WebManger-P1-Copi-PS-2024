05eab2f70af9798da86a7b89195708b66efac040
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2024-04-14 13:26:22 -0600

    Client: inicio

---
+++ WebManager_Client/src/main/java/com/jbrod/webmanager_client/WebManager_Client.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jbrod.webmanager_client;
+
+import com.jbrod.webmanager_client.ui.Mainframe;
+
+/**
+ *
+ * @author jbravo
+ */
+public class WebManager_Client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Mainframe mainframe = new Mainframe();
+        mainframe.setVisible(true);
+    }
+}
